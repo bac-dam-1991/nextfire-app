a0d9c5d836a5fd98d9bae78f863bf316d59a329f
Bac Dam <bac@peakdigitalstudio.com>
2021-07-26 20:50:22 +1000

    Setup.

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+
+export interface NavbarProps {}
+
+const Navbar = (props: NavbarProps): ReactElement => {
+	return <nav></nav>;
+};
+
+export default Navbar;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export interface HomePageProps {}
+
+const HomePage = (props: HomePageProps) => {
+	return <div>Hello</div>;
+};
+
+export default HomePage;
